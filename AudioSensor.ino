d8fe74c78ee9d5287120478a28e027823c75f304
Michael Vogt <michu@neophob.com>
2011-12-05 22:48:17 +0100

    update audio stuff

--- AudioSensor.ino
+++ AudioSensor.ino
@@ -1,11 +1,18 @@
 
+//TODO: maybe we need to smooth the values (arduino example)
 void loopAudioSensor() {
   uint16_t sensorValue = analogRead(A0);//use A0 to read the electrical signal
   if (sensorValue > maxVal) {
     maxVal = sensorValue;
   }
   
+  //calculate current volume vs. maximal volume
   audioVol = (1.0f/maxVal)*sensorValue;
+  
+  //decrease maximum volume, adjust it
+  if (maxVal>1) {
+    maxVal--;  
+  }
   
 #ifdef USE_SERIAL_DEBUG
   Serial.print("sensorValue: ");
