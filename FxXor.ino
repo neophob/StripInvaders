bb56d483a985345322e5d9c931e5a547061a3cb9
Michael Vogt <michu@neophob.com>
2011-12-17 12:16:02 +0100

    minor typos

--- FxXor.ino
+++ FxXor.ino
@@ -21,24 +21,24 @@ void loopXor() {
 
       case 1:    
           xorR = (ofs^4)%256;
-          xorG = ((ofs>>1)^frame)%256;
-          xorB = (r^g)%256;
+          xorG = ((ofs>>1)^frames)%256;
+          xorB = (xorR^xorG)%256;
           break;
           
       case 2:
           xorR = (ofs^i)%256;
           xorG = (ofs^i)%256;
-          xorB = (ofs^r)%256;
+          xorB = (ofs^xorR)%256;
           break;
           
       case 3:
-          xorR = (i*(frame>>1))%256;
+          xorR = (i*(frames>>1))%256;
           xorG = (i^ofs)%256;
           xorB = (ofs)%256;
           break;
           
       case 4:
-          xorR = (frame*i)%256;
+          xorR = (frames*i)%256;
           xorG = xorR;
           xorB = xorR;
           break;
@@ -47,7 +47,7 @@ void loopXor() {
           ofs >>= 3;
           xorR = (ofs*ofs)%256;
           xorG = (i*ofs>>1)%256;
-          xorB = ((frame>>3)*ofs>>1)%256;
+          xorB = ((frames>>3)*ofs>>1)%256;
           break;
 
     }    
